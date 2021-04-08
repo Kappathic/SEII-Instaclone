2bc69fa2ba342aa666e80ff02e72dbfe3e2a22ca
Kappathic <gralwe@web.de>
2021-04-08 08:29:10 +0200

    Added first Concept of post Component

---
+++ frontend/src/app/post-container/post-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-container',
+  templateUrl: './post-container.component.html',
+  styleUrls: ['./post-container.component.scss']
+})
+export class PostContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
