a338dbdd00b131223ef9c2d0e7c78095a4c41769
NKJN01 <60649390+NKJN01@users.noreply.github.com>
2021-04-27 22:28:50 +0200

    Logout now working

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SnackBarService } from '../snack-bar-service.service';
-import {CookieService} from 'ngx-cookie-service';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,7 +11,24 @@ import {CookieService} from 'ngx-cookie-service';
 export class HeaderComponent implements OnInit {
   constructor(
     private snackBar: SnackBarService,
-    private cookieService: CookieService) { }
+    private http: HttpClient,
+    private router: Router
+  ) {}
+  submitLogout(): void {
+    const requestUrl = 'api/auth/logout';
+    this.http.post(requestUrl, {
+    }).subscribe(
+      (data: any) => {
+        console.log(data);
+        console.log('successfully Logged out!');
+        this.snackBar.open('Successfully Logged Out!', 'Close');
+        this.router.navigate(['home']);
+      },
+      (error) => {
+          this.snackBar.open('Something went wrong!', 'Close');
+      }
+    );
+  }
   ngOnInit(): void {
   }
 }
