96dd4fb7d10c907651794044286509b1ee5c9fa1
Kappathic <gralwe@web.de>
2021-04-28 20:20:32 +0200

    Added Main Feed backend get request

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {SnackBarService} from "../snack-bar-service.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private snackBar: SnackBarService) { }
+  loadPosts(): void{
+    const requestUrl = 'api/post/feed';
+    this.http.get(requestUrl, {}).subscribe(
+      (data: any) => {
+        console.log(data);
+        this.snackBar.open('Mainfeed loaded.', 'close');
+      },
+      (error) => {
+        switch (error.status) {
+          case 401:
+            console.log('Wrong credentials');
+            this.snackBar.open('Wrong credentials', 'close');
+            break;
+          case 404:
+            console.log('User not Found!');
+            this.snackBar.open('User not Found', 'close');
+            break;
+          default:
+            console.log('Bad Request');
+            this.snackBar.open('Bad Request', 'close');
+            break;
+        }
+      }
+    );
+}
   ngOnInit(): void {
+    this.loadPosts();
   }
 
 }
